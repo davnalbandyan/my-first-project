5ea9adfd8949d89271767372188bf29a2d86c70c
davnalbandyan <davnalbandyan@gmail.com>
2023-07-13 13:06:58 +0400

    add the new timer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
+
 window.addEventListener("load",()=>{
+    //tab logic start
 const tabHeaders=document.querySelectorAll(".tabheader__item");
 const tabContents=document.querySelectorAll(".tabcontent");
 const tabsParent=document.querySelector(".tabheader__items");
@@ -36,8 +38,59 @@ tabsParent.addEventListener("click",(e)=>{
     })
    }
 })
-})
+//tab logic end
 
+//timer logic start
+const dateLine="2023-09-01"
+function getTimeRemaining(end){
+const total =Date.parse(end)-Date.parse(new Date())
+const days=Math.floor(total / (1000 *60*60*24));/* get the days quantity*/
+const hours=Math.floor((total / (1000 * 60 * 60) % 24));/*get the hours quantity*/
+const min=Math.floor((total / 1000 /60)%60)/* get the min quantity*/
+const seconds=Math.floor((total / 1000 )% 60);/* get the sec quantity*/
+
+return {
+    total,
+    days,
+    hours,
+    min,
+    seconds
+};
+}
+function setClock(selector,ending){
+    const timer =document.querySelector(selector);
+    const daysBlock=timer.querySelector("#days");
+    const hoursBlock=timer.querySelector("#hours");
+    const minutesBlock=timer.querySelector("#minutes");
+    const secondsBlock=timer.querySelector("#seconds");
+    const timerID=setInterval(updateClock,1000)
+    updateClock()
+    function updateClock(){
+        const time=getTimeRemaining(ending);  /*ask to Sammi ending ի պահ ը)*/
+
+        daysBlock.textContent = time.days >=0 && time.days <10? `0${time.days}`: time.days
+        hoursBlock.textContent=time.hours >=0 && time.hours <10? `0${time.hours}`: time.hours;
+        minutesBlock.textContent=time.min >=0 && time.minutes <10? `0${time.min}`:time.min;
+        secondsBlock.textContent=time.seconds >0 && time.seconds <10? `0${time.seconds}`: time.seconds;
+
+
+
+        if(time.total <= 0){
+            clearInterval(timerID)
+        };
+
+
+    };
+};
+
+setClock(".timer",dateLine)
+//timer logic end
+
+
+
+
+
+})
 
 
 
